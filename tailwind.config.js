9ecc209682421a3f049c3f76e80c09767cf08fbd
Ish-Avanzado <isiaavanzado@gmail.com>
2025-04-14 16:03:19 +0800

    updating files

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+      backgroundImage: {
+        homeBanner: "url(../dist/img/banner.jpg)"
+      },
+      colors: {
+        dark: "#2b2b2b",
+        light: "#ffffff",
+        darkblue: "#041341",
+        lightblue: "#1388ff",
+        orange: "#fd872f",
+        blue: "#0f5fff",
+        darkgray: "#636363",
+        asul: "#000e52",
+        buttonDarkBlue: "#1060ff",
+        buttonLightBlue: "#1384ff",
+        buttonDarkOrange: "#ff630c",
+        buttonLightOrange: "#fe940d",
+      },
+    },
+  },
+  plugins: [],
+};
+
